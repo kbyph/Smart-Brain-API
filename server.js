7246c291d23af1ae6e3bd93e8548ca98ba7298ea
kbyph <kobeypho@gmail.com>
2023-05-09 14:46:13 -0700

    Changing Response

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send(db.users) })
+app.get('/', (req, res) => { res.send('It is working!') })
 
 // -------------------------------------------------------
 // SIGN IN ENDPOINT
